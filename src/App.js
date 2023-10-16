82a2dfe2a37aca65b7172ab6e7b902162ae55d59
Ocean <zouhaiyang2005@gmail.com>
2023-10-17 10:07:44 +1100

    reture back without react-device-detect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import {isMobile} from 'react-device-detect'
 import BackgroundImage from './component/BackgroundImage/BackgroundImage';
 import WeatherCard from './component/WeatherCard'
 import bg from './asset/bg-img.webp'
 
 function App() {
-    if(isMobile){
-    return <div>hello world11</div>
-  }else{
   return (
     <BackgroundImage image={bg} >
       <div className='h-screen flex justify-center items-center'>
         <WeatherCard/>
       </div>
-    </BackgroundImage>)}
+    </BackgroundImage>)
 }
 
 export default App;
