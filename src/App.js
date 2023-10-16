face04068c810126db755842c104126ce3faaf02
Ocean <zouhaiyang2005@gmail.com>
2023-10-17 10:00:22 +1100

    add isMobileTest

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import WeatherCard from './component/WeatherCard'
 import bg from './asset/bg-img.webp'
 
 function App() {
+    if(isMobile){
+    return <div>hello world11</div>
+  }else{
   return (
     <BackgroundImage image={bg} >
       <div className='h-screen flex justify-center items-center'>
         <WeatherCard/>
       </div>
-    </BackgroundImage>)
+    </BackgroundImage>)}
 }
 
 export default App;
